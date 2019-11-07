ee7ad361bcb66dc9fee0e50bdb051056b60750be
shark <farzamzohdi@gmail.com>
2019-11-07 19:08:21 +0330

    login works fine, problem is sign up

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
 
-from main.views import main_page, signup, login
+from main.views import main_page, sign_up, log_in
 
 urlpatterns = [
     path('', main_page, name='homePage'),
-    path('signup/', signup, name='signupPage'),
-    path('login/', login, name='loginPage'),
+    path('signup/', sign_up, name='signupPage'),
+    path('login/', log_in, name='loginPage'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,24 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 from main.forms import SignUpForm
 
 
 def base(request):
-    return render(request, 'base.html')
+    context = {'signed_in': request.user.is_authenticated,
+               }
+    return render(request, 'base.html', context=context)
 
 
 def main_page(request):
-    return render(request, 'mainPage.html')
+    context = {'signed_in': request.user.is_authenticated,
+               }
+    return render(request, 'mainPage.html', context=context)
 
 
-def signup(request):
+def sign_up(request):
     if request.method == 'POST':
         signupForm = SignUpForm(request.POST)
         if signupForm.is_valid():
@@ -21,8 +26,22 @@ def signup(request):
             return redirect('homePage')
     else:
         signupForm = SignUpForm()
-    return render(request, 'signup.html', {'signupForm': signupForm})
+    context = {'signed_in': request.user.is_authenticated,
+               'signupForm': signupForm,
+               }
+    return render(request, 'signup.html', context=context)
 
 
-def login(request):
-    pass
+def log_in(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('homePage')
+        else:
+            return redirect('loginPage')
+    context = {'signed_in': request.user.is_authenticated,
+            }
+    return render(request, 'login.html', context=context)
