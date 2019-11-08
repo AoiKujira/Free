dadeff26453de7a9e9094a9b6a43edc6cc6af962
shark <farzamzohdi@gmail.com>
2019-11-08 07:44:06 +0330

    q10

--- main/views.py
+++ main/views.py
@@ -1,10 +1,7 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
 from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 
 from main.forms import *
 
