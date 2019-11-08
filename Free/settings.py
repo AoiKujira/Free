7e84a6c797c5dee9dc83c7f2856c3c5aaa9f25c1
shark <farzamzohdi@gmail.com>
2019-11-08 09:59:58 +0330

    q9 edited

--- Free/settings.py
+++ Free/settings.py
@@ -116,3 +116,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'farzamzohdi@gmail.com'
+# EMAIL_HOST_PASSWORD = 'password'
+# EMAIL_PORT = 587
--- main/forms.py
+++ main/forms.py
@@ -22,5 +22,5 @@ class ContactForm(forms.Form):
 
 
 class ProfileEditForm(forms.Form):
-    new_first_name = forms.CharField(max_length=30, required=False)
-    new_last_name = forms.CharField(max_length=30, required=False)
+    new_first_name = forms.CharField(max_length=200, required=False)
+    new_last_name = forms.CharField(max_length=200, required=False)
